81de74b2383ac26627d0f9e245fd4517110c17a1
svanlaere <svanlaere@users.noreply.github.com>
2020-08-23 23:03:32 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -5,14 +5,15 @@ SET binpath=%workpath%\bin
 SET avif=%binpath%\avif.exe
 IF NOT DEFINED args GOTO END
 PUSHD %args%
-RENAME *.JPG *.jpg
 ECHO Converting has started (it might take some time)
 ECHO.
 FOR /R %%A IN (*.jpg) DO (
+IF NOT EXIST "%%~pnA.avif" (
     %avif% -e %%~A -o %%~pnA.avif
 	ECHO Saved converted file:
 	ECHO %%~nA.avif
 	ECHO.
+  )
 )
 ECHO Converting has finished
 ECHO.
